49031fa24476e0ebd48d8468752cc8036a378336
Henry Darana <henry.darana@tray.com>
2023-03-27 17:40:50 +0530

    Removed three schedules, created a 100 run test case.

--- src/test/java/GitHubProfileLookUpTest.java
+++ src/test/java/GitHubProfileLookUpTest.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -11,40 +14,49 @@ import java.time.Duration;
 public class GitHubProfileLookUpTest {
 
     public static final String urlGithubProfile = "https://github.com/Henry-Darana";
+    public static final String urlDrinkHaus = "https://drink.haus/";
     By lblNameLocators = By.xpath("//span[contains(text(), 'Henry-Darana')]");
+    WebDriver driver;
 
-    @Test
-    public void visitGitHubProfile() {
-
-        WebDriverManager
-                .chromedriver()
-                .setup();
+    @BeforeSuite
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
 
         ChromeOptions cOptions = new ChromeOptions();
         cOptions.addArguments("--remote-allow-origins=*");
         cOptions.addArguments("--headless");
         cOptions.addArguments("--disable-gpu");
-        WebDriver driver = new ChromeDriver(cOptions);
+        driver = new ChromeDriver(cOptions);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
-        driver
-                .manage()
-                .window()
-                .maximize();
-
-        driver
-                .manage()
-                .timeouts()
-                .implicitlyWait(Duration.ofSeconds(10));
-
-        driver.get(urlGithubProfile);
-
-        int actual = driver
-                .findElements(lblNameLocators)
-                .size();
-
-        Assert.assertTrue(actual > 0);
+    @AfterSuite
+    public void tearDown() {
 
         driver.close();
+    }
 
+    @Test(dataProvider = "hack-provider")
+    public void visitGitHubProfile(int i) {
+
+        driver.get(urlGithubProfile);
+        int actual = driver.findElements(lblNameLocators).size();
+        Assert.assertTrue(actual > 0);
+
+        driver.get(urlDrinkHaus);
+        String pageTitle = driver.getTitle();
+        System.out.println(pageTitle);
+        Assert.assertTrue(pageTitle.toLowerCase().contains("haus"));
+
+    }
+
+    @DataProvider(name = "hack-provider")
+    public Object[][] hackProvider() {
+        Object[][] counter = new Object[100][1];
+        for (int i = 0; i < 100; i++) {
+            counter[i][0] = i;
+        }
+        return counter;
     }
 }
